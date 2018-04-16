c67d9e50d5bf57fcefadf76e83f1bd6446123b3e
hauntedlollipop <36665442+hauntedlollipop@users.noreply.github.com>
2018-04-16 18:30:06 +0200

    Update API_key.php

--- scripts/API_key.php
+++ scripts/API_key.php
@@ -3,6 +3,7 @@ require_once("scripts/IRequest.php");
  class Authorization implements IRequest {
 
 	public $API_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJiY2UxMzQ1MC0xYmM4LTAxMzYtMzRhZC0wMzExMWE4MzhiNmMiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTIzMDE5NzcxLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InB1Ymctc3RhdHMtd2Vic2l0ZSIsInNjb3BlIjoiY29tbXVuaXR5IiwibGltaXQiOjEwfQ.miREOEm4iMfNhTr4aPFWFJ4lgouCU0Q3JUfXPwOHbJc";
+	public static $authenticated = false;
 	public $api_url;
 
 
@@ -12,19 +13,35 @@ require_once("scripts/IRequest.php");
 	}
 	public function httpRequest($api, $url)
 	{
-		$opts = array(
-		  'http'=>array(
-		    'method'=>"GET",
-		    'header'=>"Authorization:".$api."\r\n" .
-		              "Accept: application/vnd.api+json\r\n"
-		  )
-		);
+		try {
+			$opts = array(
+			  'http'=>array(
+			    'method'=>"GET",
+			    'header'=>"Authorization:".$api."\r\n" .
+			              "Accept: application/vnd.api+json\r\n"
+			  )
+			);
 
-		$context = stream_context_create($opts);
-		$file = file_get_contents($url, false, $context);
-		$obj = json_decode($file, true);
+			$context = stream_context_create($opts);
+			if($context === true)
+			{
+			$file = file_get_contents($url, false, $context);
+			$obj = json_decode($file, true);
+			Authorization::$authenticated = true;
+			return $obj;
+			}
+			else 
+			{
+				echo "Error! Requsted file was not found!";
+			}
+			
 
-		return $obj;
+		}
+
+		catch(Exception $e)
+		{
+			var_dump("dsadsad");
+		}
 	}
 }
 ?>
