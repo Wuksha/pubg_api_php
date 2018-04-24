a9b7033a2aa24f78e3e04135d4f184f2e53592f0
hauntedlollipop <36665442+hauntedlollipop@users.noreply.github.com>
2018-04-24 22:12:57 +0200

    Update httpReq.php

--- scripts/httpReq.php
+++ scripts/httpReq.php
@@ -3,7 +3,7 @@ require_once("scripts/IRequest.php");
  class Authorization implements IRequest {
 
 	public $API_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJiY2UxMzQ1MC0xYmM4LTAxMzYtMzRhZC0wMzExMWE4MzhiNmMiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTIzMDE5NzcxLCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InB1Ymctc3RhdHMtd2Vic2l0ZSIsInNjb3BlIjoiY29tbXVuaXR5IiwibGltaXQiOjEwfQ.miREOEm4iMfNhTr4aPFWFJ4lgouCU0Q3JUfXPwOHbJc";
-	public static $authenticated = false;
+	public $authenticated = false;
 	public $api_url;
 
 
@@ -23,24 +23,25 @@ require_once("scripts/IRequest.php");
 			);
 
 			$context = stream_context_create($opts);
-			if($context === true)
+			@$file = file_get_contents($url, false, $context);
+			if($file == false)
 			{
-			$file = file_get_contents($url, false, $context);
-			$obj = json_decode($file, true);
-			Authorization::$authenticated = true;
-			return $obj;
+				throw new Exception("Player unknown");
+			
 			}
-			else 
-			{
-				echo "Error! Requsted file was not found!";
-			}
+				else 
+				{
+				$obj = json_decode($file, true);
+				$this->authenticated = true;
+				return $obj;
+				}
 			
 
 		}
 
 		catch(Exception $e)
 		{
-			var_dump("dsadsad");
+			echo ($e->getMessage());
 		}
 	}
 }
