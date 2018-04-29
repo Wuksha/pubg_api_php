eb0cc149e21793ccf36b29522627a42e1f4c2129
hauntedlollipop <36665442+hauntedlollipop@users.noreply.github.com>
2018-04-29 17:13:52 +0200

    Add files via upload

--- playerInfo.php
+++ playerInfo.php
@@ -10,12 +10,13 @@ $playerName = end($link_array);
 
 $player = new playerData($playerName);
 $matches_array = $player->getData();
+ChromePhp::log($matches_array);
 
-for($i = 1; $i <= 2; $i++)
+for($i = 1; $i <= 3; $i++)
 {
 
     $match = new matchData($matches_array["Match ".$i]);
     $match_deatils = $match->getData();
-    ChromePhp::log($match_deatils);
+    var_dump($match_deatils);
 }
 ?>
